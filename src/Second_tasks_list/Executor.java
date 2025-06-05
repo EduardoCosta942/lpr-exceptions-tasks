0a6a3e29a2de16e2efe2fd05063783ce989edf6d
Eduardo Costa <eduardocosta942z@gmail.com>
2025-06-05 09:55:22 -0300

    tasks 1, 2 and 3 at Second_tasks_list/Methods plus main for excercises 1 and 2

--- src/Second_tasks_list/Executor.java
+++ src/Second_tasks_list/Executor.java
@@ -6,17 +6,35 @@ import java.util.Scanner;
 
 public class Executor {
     public static void main(String[] args) {
+        // Vars:
+        boolean again = true;
         Scanner scanner = new Scanner(System.in);
         int n;
         String dayOfTheWeek = "";
+        int[] triangleSides = new int[3];
+
+        // Main for exercise 1:
+        do {
+            again = false;
+            try {
+                for (int v = 0; v < triangleSides.length; v++) triangleSides[v] = scanner.nextInt();
+                System.out.println(Methods.triangleType(triangleSides[0], triangleSides[1], triangleSides[2]));
+            } catch (InputMismatchException | IllegalArgumentException ime_iae) {
+                System.out.println(ime_iae);
+                again = true;
+            }
+        } while (again);
+
+        // Main for exercise 3:
         do {
             try {
                 n = scanner.nextInt();
                 dayOfTheWeek = Methods.weekDay((byte) n);
                 System.out.println(dayOfTheWeek);
-            } catch (IllegalArgumentException | InputMismatchException iae) {
-                System.out.println("An error occurred: " + iae);
+            } catch (IllegalArgumentException | InputMismatchException iaeIme) {
+                System.out.println("An error occurred: " + iaeIme);
             }
+            scanner.next();
         } while (!dayOfTheWeek.equals("end"));
     }
 }
--- src/Second_tasks_list/Methods.java
+++ src/Second_tasks_list/Methods.java
@@ -1,13 +1,15 @@
 package Second_tasks_list;
-import java.time.temporal.ChronoUnit;
+//import java.time.temporal.ChronoUnit;
 import java.time.LocalDate;
 import java.util.InputMismatchException;
 
 public class Methods {
     public static String triangleType(double side1, double side2, double side3) throws IllegalArgumentException {
-        // Validating if triangle is true:
+        // Validations:
         if (side3 + side2 < side1 || side1 + side2 < side3 || side3 + side1 < side2){
-            throw new IllegalArgumentException("Those values do not create a triangle"); // Throwing "iae" exception
+            throw new IllegalArgumentException("There is no triangle"); // Throwing iae exception if triangle is not true;
+        } else if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
+            throw new IllegalArgumentException("Values under 0"); // Throwing iae exception if values under 0
         }
 
         // Searching triangle type
@@ -19,19 +21,19 @@ public class Methods {
         return "Isósceles";
     }
 
-    public static double disccount(LocalDate signUp_date, double value, int boughtsAtLastMonth) throws InputMismatchException{
-        int disccount = 0;
-        if (ChronoUnit.YEARS.between(LocalDate.now(), signUp_date) > 10) {
-            disccount+=15;
-        }
-        if (value > 1500) {
-            disccount+=17;
-        }
-        if (boughtsAtLastMonth > 5) {
-            disccount+=13;
-        }
-        return disccount;
-    }
+//    public static double disccount(LocalDate signUp_date, double value, int boughtsAtLastMonth) throws InputMismatchException{
+//        int disccount = 0;
+//        if (ChronoUnit.YEARS.between(LocalDate.now(), signUp_date) > 10) {
+//            disccount+=15;
+//        }
+//        if (value > 1500) {
+//            disccount+=17;
+//        }
+//        if (boughtsAtLastMonth > 5) {
+//            disccount+=13;
+//        }
+//        return disccount;
+//    }
 
     public static String weekDay(byte targetDay) throws IllegalArgumentException{
         // Declaring vars:
@@ -42,6 +44,7 @@ public class Methods {
 
         if (targetDay == 0) return "end"; // Ending program
         if (targetDay < 1 || targetDay > 7) throw new IllegalArgumentException("Parameter must be in range (1 to 7)"); // Throwing IAE according to condition
+
         return daysOfTheWeek[targetDay - 1]; // If there is no IAE, returning the correct String.
     }
 }
